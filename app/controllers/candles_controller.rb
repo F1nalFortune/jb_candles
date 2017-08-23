99c41d66ef56d4b69b92a8ec9bfe67bf0b45a962
Brian <juniortoad1738@gmail.com>
2017-08-22 22:21:31 -0400

    candle controller / landing page

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -7,7 +7,23 @@ class CandlesController < ApplicationController
     @candle = Candle.find(params[:id])
   end
 
+  def create
+    @candle = Candle.new(candle_params)
+
+    if @candle.save
+      redirect_to candles_path
+    else
+      render :new
+    end
+  end
+
   def new
     @candles = Candle.new
   end
+
+def candle_params
+  params.require(:candle).permit(:title, :body, :price)
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'candles#index'
   resources :candles
 end
