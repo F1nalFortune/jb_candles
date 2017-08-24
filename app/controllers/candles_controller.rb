0234eb42c0a4b82a24a0267682413e68f580bd37
Brian <juniortoad1738@gmail.com>
2017-08-23 23:40:38 -0400

    add/delete candle function

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -9,7 +9,6 @@ class CandlesController < ApplicationController
 
   def create
     @candle = Candle.new(candle_params)
-
     if @candle.save
       redirect_to candles_path
     else
@@ -21,9 +20,31 @@ class CandlesController < ApplicationController
     @candles = Candle.new
   end
 
-def candle_params
-  params.require(:candle).permit(:title, :body, :price)
-end
+  def edit
+    @candle = Candle.find(params[:id])
+  end
+
+  def update
+    @candle = Candle.find(params[:id])
+
+    if @candle.update(candle_params)
+      redirect_to candles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candle = Candle.find(params[:id])
+    @candle.destroy
+    redirect_to candles_path
+  end
+
+
+
+  def candle_params
+    params.require(:candle).permit(:title, :body, :price)
+  end
 
 
 end
